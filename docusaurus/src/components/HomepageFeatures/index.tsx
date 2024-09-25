c6f32ff9d715ca24e6be134394e5dfd652ea42db
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2024-09-25 14:07:21 +0200

    Refactor: HomepageFeatures component to add documentation links and changed style.

--- docusaurus/src/components/HomepageFeatures/index.tsx
+++ docusaurus/src/components/HomepageFeatures/index.tsx
@@ -7,6 +7,8 @@ type FeatureItem = {
   imgSrc: string;
   description: JSX.Element;
   link: string;
+  docs: string;
+  brandColor: string;
 };
 
 const FeatureList: FeatureItem[] = [
@@ -18,7 +20,9 @@ const FeatureList: FeatureItem[] = [
         EasyThreads can be used for autothreading, moving of threads, ticket-systems and much more!
       </>
     ),
-    link: 'https://ezsys.link/threads'
+    link: 'https://ezsys.link/threads',
+    docs: '/docs/easythreads/intro',
+    brandColor: '#4ea4df',
   },
   {
     title: 'EasyLevel',
@@ -28,7 +32,9 @@ const FeatureList: FeatureItem[] = [
         With EasyLevel you can customize almost every aspect of your Leveling experience with ease.
       </>
     ),
-    link: 'https://ezsys.link/level'
+    link: 'https://ezsys.link/level',
+    docs: '/docs/easylevel/intro',
+    brandColor: '#98ce86',
   },
   {
     title: 'EasyVoice',
@@ -38,7 +44,9 @@ const FeatureList: FeatureItem[] = [
         EasyVoice is the best app for temporary Voice Channels!
       </>
     ),
-    link: 'https://ezsys.link/voice'
+    link: 'https://ezsys.link/voice',
+    docs: '/docs/easyvoice/intro',
+    brandColor: '#a1a7f6',
   },
   {
     title: 'EasyYAUDAB',
@@ -48,11 +56,13 @@ const FeatureList: FeatureItem[] = [
         Hello! Chat with EasyYAUDAB by mentioning him and writing your message!
       </>
     ),
-    link: 'https://ezsys.link/yaudab'
+    link: 'https://ezsys.link/yaudab',
+    docs: '/docs/easyyaudab/intro',
+    brandColor: '#f16aec',
   },
 ];
 
-function Feature({title, imgSrc, description, link}: FeatureItem) {
+function Feature({title, imgSrc, description, link, docs, brandColor}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -62,9 +72,25 @@ function Feature({title, imgSrc, description, link}: FeatureItem) {
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
-      <div className="text--center padding-horiz--md">
-        <a href={link} className="button button--secondary button--lg">
+      <div className={styles.buttonContainer}>
+        <a 
+          href={link} 
+          className={`button ${styles.addToServerButton}`}
+          style={{
+            backgroundColor: brandColor,
+          }}
+        >
           Add to Server
+        </a>
+        <a 
+          href={docs} 
+          className={`button ${styles.docsButton}`} 
+          style={{
+            borderColor: brandColor,
+            color: brandColor,
+          }}
+        >
+          VIEW DOCUMENTATION
         </a>
       </div>
     </div>
